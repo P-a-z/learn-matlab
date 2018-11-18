b29a984cb04eadae7168f151f678b0f1624c7506
P-a-z <32504499+P-a-z@users.noreply.github.com>
2018-11-18 20:32:55 +0800

    Add files via upload

--- createpng.m
+++ createpng.m
@@ -4,5 +4,9 @@ T = countEachLabel(imds);
 % 每次把一个文件夹中的.pgm转化为.png
 for i = 1: 10
     image = readimage(imds, i);
-    imwrite(image, ['.\photo\1\', int2str(i), '.png']);
+    if i <= 8
+        imwrite(image, ['.\photo\1\', int2str(i), '.png']);
+    else
+        imwrite(image, ['.\test\1\', int2str(i), '.png']);
+    end
 end
